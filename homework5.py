a5903d98e2bd704c2f5fa651276c12cf47ccb37d
MarinaIgnat <9702m@mail.ru>
2024-07-01 22:10:51 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+#1
+immutable_var = tuple = 8, 'mounth', 0
+print(immutable_var)
+
+#2
+#tuple[2]= 'december'
+#print(immutable_var)
+#значение элементов невозможно изменить, тк tuple является неизменяемым типом
+
+#3
+mutable_list = tuple = 'invoice', ' ', 768, [6, 'time', 84]
+tuple[3][1] = 'clock'
+print(mutable_list)
+
